89ba6d9634f4c9b6a29f3b0d153626e7602a379a
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-09-05 19:58:19 +0530

    Time: 133 ms (42.48%) | Memory: 73.5 MB (8.45%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        set<int>s(nums.begin(),nums.end());
+        if(s.size()==n)
+            return false;
+        return true;
+    }
+};
