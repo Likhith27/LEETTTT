ce1de4682f09e7575e2baa64f2dc3c33bec0a73c
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-09-05 21:25:09 +0530

    Time: 4 ms (32.51%) | Memory: 6.1 MB (32.28%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) 
+    {
+       int count = 0;
+    
+        while (n) 
+        {
+            n &= (n - 1);
+            count++;
+        }
+        
+        return count; 
+        // return __builtin_popcount(n);
+    }
+    // trick is by the time n reaches zero number of times we do & between n and n-1 is the number of ones in the given number
+};
