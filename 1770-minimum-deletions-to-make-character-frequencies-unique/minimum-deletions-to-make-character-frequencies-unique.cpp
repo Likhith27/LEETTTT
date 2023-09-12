d7a4b4291fb627f30e68ce4827a73369a2f741c8
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-09-12 11:03:56 +0530

    Time: 50 ms (77.52%) | Memory: 17.2 MB (92.40%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int>v(26,0);
+        for(char c:s)
+        {
+            v[c-'a']++;
+        }
+        sort(v.begin(),v.end());
+        int cnt=0;
+        for(int i=24;i>=0;i--)
+        {
+            if(v[i]==0)
+                break;
+            if(v[i]>=v[i+1])
+            {
+                int prev = v[i];
+                v[i]=max(0,v[i+1]-1);
+                cnt+=prev-v[i];
+            }
+        }
+        return cnt;
+
+
+    }
+};
