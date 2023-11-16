966c6946f1d7c16286c4adde653076937bdef08a
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-11-16 18:17:04 +0530

    Time: 3 ms (74.62%) | Memory: 10.5 MB (86.71%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -4,6 +4,7 @@ public:
     {
         int n = nums.size();
         string s;
+        // for every i-th string we go to i-th number and add the opposite of ith number
         for(int i=0;i<n;i++)
         {
             if(nums[i][i]=='0')
