e66b88f6b656a37f8896cb8e4124c4eaed58cbec
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-11-16 14:14:18 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (93.97%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    void f(string& digits, int index, string& combination, vector<string>& mapping, vector<string>& result) 
-{
-
-    // Base case: If we have processed all digits, add the current combination to the result.
-    if(digits.size()==0)
-        return;
-    if (index == digits.length()) 
+    void f(int ind,string &digits,string &temp,vector<string>&map,vector<string>&ans)
     {
-        result.push_back(combination);
-        return;
+        if(digits.size()==0)
+            return;
+        if(ind == digits.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        string letters = map[digits[ind]-'0'];
+        for(char ch:letters)
+        {
+            temp += ch;
+            f(ind+1,digits,temp,map,ans);
+            temp.pop_back();
+        }
     }
-
-    // Get the letters corresponding to the current digit.
-    string letters = mapping[digits[index] - '0'];
-
-    // Iterate over the letters and generate combinations.
-
-    for (char letter : letters) 
-    {
-        combination += letter;
-        f(digits, index + 1, combination , mapping, result);
-        combination.pop_back();
-    }
-}
     vector<string> letterCombinations(string digits) {
-       // Mapping of digits to letters.
-    vector<string> mapping = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    // Result vector to store the generated combinations.
-    vector<string> result;
-    string combination;
-    // Call the recursive function to generate combinations.
-    f(digits, 0, combination, mapping, result);
-    // Return the result vector containing the generated combinations.
-    return result;
+        vector<string>ans;
+        vector<string>map = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string temp;
+        f(0,digits,temp,map,ans);
+        return ans;
     }
 };
