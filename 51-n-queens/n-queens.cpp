6e0dc4b50cb68c30db1bd00589f95d7174ebd3db
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-11-18 15:03:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (82.27%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void f(int col,vector<int>&left,vector<int>&lower,vector<int>&upper,vector<string>&temp,vector<vector<string>>&ans,int n)
+    {
+        if(col==n)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        for(int row=0;row<n;row++)
+        {
+            if(left[row]==0 and lower[row+col]==0 and upper[n-1+col-row]==0)
+            {
+                temp[row][col]='Q';
+                left[row]=1;
+                lower[row+col]=1;
+                upper[n-1+col-row]=1;
+                f(col+1,left,lower,upper,temp,ans,n);
+                temp[row][col]='.';
+                left[row]=0;
+                lower[row+col]=0;
+                upper[n-1+col-row]=0;
+
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>v;
+        vector<string>temp(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+        {
+            temp[i] =s;
+        }
+        vector<int>left(n,0),lower(2*n-1,0),upper(2*n-1,0);
+        f(0,left,lower,upper,temp,v,n);
+        return v;
+    }
+};
