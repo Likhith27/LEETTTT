8f366b16b88d6ed5e2a1440b87610b3fb7b08f95
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-11-22 14:18:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (26.25%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
+    int n;
+    unordered_set<string>st;
+    string f(string s)
+    {
+        if(s.size()==n)
+        {
+            if(st.find(s)==st.end())
+                return s;
+            return "";
+        }
+        string zero = f(s+"0");
+        if(zero.size()>0)
+            return zero;
+        return f(s+"1");
+        
+    }
     string findDifferentBinaryString(vector<string>& nums) 
     {
-        int n = nums.size();
-        string s;
-        // for every i-th string we go to i-th number and add the opposite of ith number
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i][i]=='0')
-                s+='1';
-            else
-                s+='0';
+        // int n = nums.size();
+        // string s;
+        // // for every i-th string we go to i-th number and add the opposite of ith number
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i][i]=='0')
+        //         s+='1';
+        //     else
+        //         s+='0';
 
-        }
-        return s;
-        
+        // }
+        // return s;
+        n = nums.size();
+        for(string s:nums)
+            st.insert(s);
+
+        return f("");
     }
 };
