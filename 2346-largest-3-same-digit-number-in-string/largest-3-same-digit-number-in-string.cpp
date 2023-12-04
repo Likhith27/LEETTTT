189a46528ed873ac5b6b31be2890bd33aaf25cc4
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2023-12-04 07:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (59.71%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        string ans="";
-    for(int i=0;i+2<num.size();i++)
-    {
-        if(num[i]==num[i+1] && num[i]==num[i+2]) 
-            ans= max(ans,{num[i],num[i+1],num[i+2]});
-    }
-    return ans;
+        vector<string>v = {"999","888","777","666","555","444","333","222","111","000"};
+        for(auto x : v)
+        {
+            if(num.find(x) != -1)
+            return x;
+        }
+        return "";
     }
 };
