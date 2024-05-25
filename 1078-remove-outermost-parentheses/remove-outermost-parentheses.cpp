0a945e8f4c485e9507339f78d65c24e8b5c796ea
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-05-25 17:10:46 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.79%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n=s.length();
+        string ans="";
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' and cnt==0)
+                cnt++;
+            else if(s[i]=='(' and cnt>=1)
+            {
+                ans += s[i];
+                cnt++;
+            }
+            else if(s[i]==')' and cnt>1)
+            {
+                ans+=s[i];
+                cnt--;
+            }
+            else if(s[i]==')' and cnt==1)
+                cnt--;
+        }
+        return ans;
+    }
+};
