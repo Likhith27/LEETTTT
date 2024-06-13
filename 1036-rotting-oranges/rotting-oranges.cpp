89c0a0fb9668973fd2455b440c15e08488868007
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-06-13 22:28:31 +0530

    Time: 12 ms (6.44%) | Memory: 16.6 MB (14.91%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,63 +1,66 @@
 class Solution {
 public:
-    int n, m;
-    vector<vector<int>> dis;
-    int dx[4] = {-1, 0, 1, 0};
-    int dy[4] = {0, 1, 0, -1};
-
-    bool is_valid(int row, int col) {
-        return row >= 0 && row < n && col >= 0 && col < m;
+    int n,m;
+    vector<vector<int>>dis;
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    bool is_valid(int row,int col)
+    {
+        if(row>=0 and row<n and col>=0 and col<m)return 1;
+        else return 0;
     }
-
-    vector<pair<int, int>> neighbor(pair<int, int> node, vector<vector<int>>& grid) {
-        vector<pair<int, int>> temp;
-        for (int i = 0; i < 4; i++) {
-            int newr = node.first + dx[i];
+    vector<pair<int,int>>neighbor(pair<int,int>node,vector<vector<int>>& grid)
+    {
+        vector<pair<int,int>>temp;
+        for(int i=0;i<4;i++)
+        {
+            int newr = node.first+dx[i];
             int newc = node.second + dy[i];
-            if (is_valid(newr, newc) && grid[newr][newc] == 1) {
-                temp.push_back({newr, newc});
-            }
+            if(is_valid(newr,newc) and grid[newr][newc]==1)
+                temp.push_back({newr,newc});
         }
         return temp;
     }
-
     int orangesRotting(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        dis.assign(n, vector<int>(m, -1));
-        queue<pair<int, int>> q;
-
-        // Initialize the queue with all the rotten oranges
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    dis[i][j] = 0;
-                    q.push({i, j});
+        dis.assign(n,vector<int>(m,-1));
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    dis[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
-
-        // Perform BFS
-        while (!q.empty()) {
+        while(!q.empty())
+        {
             auto cur = q.front();
             q.pop();
-            for (auto v : neighbor(cur, grid)) {
-                if (grid[v.first][v.second] != 2) {
-                    grid[v.first][v.second] = 2;
-                    dis[v.first][v.second] = dis[cur.first][cur.second] + 1;
+            for(auto v:neighbor(cur,grid))
+            {
+                if(grid[v.first][v.second]!=2)
+                {
+                    grid[v.first][v.second]=2;
+                    dis[v.first][v.second]=dis[cur.first][cur.second]+1;
                     q.push(v);
                 }
             }
         }
-
-        // Find the maximum distance and check if there are any fresh oranges left
         int maxi = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    return -1;  // There are still fresh oranges
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                   return -1;
                 }
-                maxi = max(dis[i][j], maxi);
+                maxi = max(dis[i][j],maxi);
             }
         }
         return maxi;
