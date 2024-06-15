6598e6ee8c1682a26a34c24ce07982d0f5daa155
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-06-15 12:53:26 +0530

    Time: 95 ms (97.04%) | Memory: 51.2 MB (91.11%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int>vis;
+    vector<int> safeNodes;
+
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& color) {
+        if (color[node] != 0) {
+            return color[node] == 2;
+        }
+        
+        color[node] = 1; // Mark the node as being processed
+
+        for (auto v : graph[node]) {
+            if (color[v] == 2) {
+                continue;
+            }
+            if (color[v] == 1 || !dfs(v, graph, color)) {
+                return false;
+            }
+        }
+        
+        color[node] = 2; // Mark the node as safe
+        safeNodes.push_back(node);
+        return true;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, 0); // 0 = unvisited, 1 = visiting, 2 = safe
+        
+        for (int i = 0; i < n; ++i) {
+            if (color[i] == 0) {
+                dfs(i, graph, color);
+            }
+        }
+        
+        sort(safeNodes.begin(), safeNodes.end());
+        return safeNodes;
+    }
+};
