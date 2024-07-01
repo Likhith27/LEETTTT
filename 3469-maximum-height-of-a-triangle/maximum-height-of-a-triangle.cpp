1f112387bba7d509c1ae06e2e9c45864a4cde608
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-07-02 01:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (31.58%) - LeetSync

--- 3469-maximum-height-of-a-triangle/maximum-height-of-a-triangle.cpp
+++ 3469-maximum-height-of-a-triangle/maximum-height-of-a-triangle.cpp
@@ -1,13 +1,39 @@
 class Solution {
-    int helper(int red,int blue){
-        int redlines=sqrt(red);
-        int bluelines=(sqrt(1+4*blue)-1)/2;
-        if(redlines==bluelines) return 2*redlines;
-        else if(redlines>bluelines) return 2*bluelines+1;
-        else return 2*redlines;
+    int maxRows(int balls) {
+        // Calculate the maximum number of complete rows that can be formed with the given balls
+        return static_cast<int>((-1 + std::sqrt(1 + 8.0 * balls)) / 2);
     }
+    
+    int helper(int red, int blue) {
+        int height = 0;
+        int row = 1;
+        bool useRed = true; // Start with red balls
+
+        while (true) {
+            if (useRed) {
+                if (red >= row) {
+                    red -= row;
+                    height++;
+                } else {
+                    break;
+                }
+            } else {
+                if (blue >= row) {
+                    blue -= row;
+                    height++;
+                } else {
+                    break;
+                }
+            }
+            useRed = !useRed;
+            row++;
+        }
+        
+        return height;
+    }
+
 public:
     int maxHeightOfTriangle(int red, int blue) {
-        return max(helper(red,blue),helper(blue,red));
+        return std::max(helper(red, blue), helper(blue, red));
     }
 };
