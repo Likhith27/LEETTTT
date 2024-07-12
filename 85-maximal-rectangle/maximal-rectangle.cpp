eac028b67161b04600d4faea1820e4a77cef95e5
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-07-13 03:44:02 +0530

    Time: 37 ms (32.54%) | Memory: 20.9 MB (25.90%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,54 @@
+class Solution 
+{
+public:
+    int f(vector<int>&heights)
+    {
+        int n = heights.size();
+
+        stack<int>st,st1;
+        vector<int>pse(n,-1),nse(n,n);
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() and heights[st.top()]>=heights[i])
+                st.pop();
+            
+            if(!st.empty())
+                pse[i]=st.top();
+            st.push(i);
+        }   
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st1.empty() and heights[st1.top()]>=heights[i])
+                st1.pop();
+            
+            if(!st1.empty())
+                nse[i]=st1.top();
+            st1.push(i);
+        }  
+        int maxi = INT_MIN; 
+        for(int i=0;i<n;i++)
+        {
+            maxi = max(maxi,(nse[i]-pse[i]-1)*heights[i]);
+        }
+        return maxi;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> row(m, 0);
+        int maxi = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') 
+                    row[j]++;
+                else 
+                    row[j] = 0;
+            }
+            maxi = max(maxi, f(row));
+        }
+        return maxi;
+    }
+};
