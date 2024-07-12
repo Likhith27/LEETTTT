3cee3cf0a07d47bbb53ffacdd60e1af06012d9c7
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-07-13 01:07:12 +0530

    Time: 195 ms (31.97%) | Memory: 137.4 MB (33.25%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
+    struct deq
+    {
+        deque<int>dq;
+    
+        void insert(int x)
+        {
+            while(!dq.empty() and dq.back()<x)
+                dq.pop_back();
+            dq.push_back(x);
+        }
+
+        void remove(int x)
+        {
+            if(dq.front()==x)
+                dq.pop_front();
+        }
+
+        int getMax()
+        {
+            return dq.front();
+        }
+    };
     vector<int> maxSlidingWindow(vector<int>& nums, int k) 
     {
         int n = nums.size();
-        vector<int> v;
-        deque<int> dq;
-        for(int i=0;i<n;i++)
+        deq dq;
+        for(int i=0;i<k;i++)
         {
-            if(!dq.empty() && dq.front()==i-k)
-                dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<nums[i])
-                dq.pop_back();
-            dq.push_back(i);
-            if(i>=k-1)
-                v.push_back(nums[dq.front()]);
+            dq.insert(nums[i]);
+        }
+        vector<int>v;
+        v.push_back(dq.getMax());
+        for(int i=0;i<n-k;i++)
+        {
+            dq.insert(nums[i+k]);
+            dq.remove(nums[i]);
+            v.push_back(dq.getMax());
         }
         return v;
     }
