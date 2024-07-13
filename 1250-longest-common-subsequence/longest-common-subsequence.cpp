6e1ab6a524f98d405c372acb48a13b01a129579a
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-07-13 18:48:56 +0530

    Time: 31 ms (37.96%) | Memory: 12.3 MB (78.82%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int f(int ind1,int ind2,string &s1,string &s2,vector<vector<int>>&dp)
+    //form-3;
+    string s1,s2;
+    int n,m;
+    int dp[1001][1001];
+    int f(int ind1,int ind2)
     {
-        if(ind1==0 || ind2==0)
+        //base case
+        if(ind1>=n || ind2>=m)
             return 0;
-        if(ind1<0 || ind2<0 || ind1>s1.size()-1||ind2>s2.size()-1)
-            return 0;
+        
+        //cache check
         if(dp[ind1][ind2]!=-1)
             return dp[ind1][ind2];
-        int match=0,not_match=0;
-        if(s1[ind1]==s2[ind2])
-            return 1+f(ind1-1,ind2-1,s1,s2,dp);
-        return max(f(ind1-1,ind2,s1,s2,dp),f(ind1,ind2-1,s1,s2,dp));
         
+        //compute
+        //match
+        int ans =0;
+        if(s1[ind1]==s2[ind2])
+            ans = 1+f(ind1+1,ind2+1);
+        else
+            ans = max(f(ind1+1,ind2),f(ind1,ind2+1));
+        
+        //cache check
+        return dp[ind1][ind2] = ans;
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int j=0;j<m;j++)
-        {
-            dp[0][j]=0;
-        }
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0]=0;
-        }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(text1[i-1]==text2[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n][m];
+        memset(dp,-1,sizeof(dp));
+        s1 = text1;
+        s2 = text2;
+        n = s1.length();
+        m = s2.length();
+        return f(0,0);
     }
 };
