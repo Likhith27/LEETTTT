93d879f88d3feef82e3aa834e6eb9a4c6fd11582
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-07-13 19:45:43 +0530

    Time: 47 ms (13.51%) | Memory: 12.5 MB (76.88%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string s1, s2;
+    int n, m, k;
+    // vector<string> v;
+    int dp[1001][1001];
+    
+    int f(int ind1, int ind2) {
+        // base case
+        if (ind1 >= n || ind2 >= m)
+            return 0;
+        
+        // cache check
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+        
+        // compute
+        int ans = 0;
+        if (s1[ind1] == s2[ind2])
+            ans = 1 + f(ind1 + 1, ind2 + 1);
+        else
+            ans = max(f(ind1 + 1, ind2), f(ind1, ind2 + 1));
+        
+        // cache the result
+        return dp[ind1][ind2] = ans;
+    }
+    int minInsertions(string s) {
+        s1 = s;
+        string t=s;
+        reverse(t.begin(),t.end());
+        s2 =t;
+
+        n = s1.size();
+        m = s2.size();
+        memset(dp,-1,sizeof(dp));
+        int k = f(0,0);//lps
+        return n-k;
+
+    }
+};
