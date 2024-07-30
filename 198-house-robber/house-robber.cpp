213b3c41bc4aad28f0ddd342a9e79fe02c919520
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-07-30 16:05:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (16.90%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    // int f(int ind,vector<int>&nums,vector<int>&dp)
-    // {
-    //     if(ind==0)
-    //         return nums[ind];
-    //     if(dp[ind]!=-1)
-    //         return dp[ind];
-    //     int pick=0,nott=0;
-    //     if(ind-2>=0)
-    //         pick = nums[ind]+f(ind-2,nums,dp);
-    //     else
-    //         pick=0;
-    //     if(ind-1>=0)
-    //         nott = f(ind-1,nums,dp);
-    //     else
-    //         nott=0;
-    //     return dp[ind]=max(pick,nott);
-    // }
-    int f(vector<int>& A, vector<int>& dp, int i) {
-        if(i >= size(A)) return 0;
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = max(f(A, dp, i+1), A[i] + f(A, dp, i+2));
+    int n;
+    vector<int>dp;
+    int f(int ind,vector<int>& nums)
+    {
+        //pruning case
+        if(ind<0)return 0; 
+        //base case
+        if(ind==0)return nums[ind]; 
+        //cache check
+        if(dp[ind]!=-1)return dp[ind];
+        //compute
+        int not_take = f(ind-1,nums);
+        int take = f(ind-2,nums)+nums[ind];
+        //save and return
+        return dp[ind]=max(take,not_take);
     }
-    int rob(vector<int>& nums) {
-        // int n = nums.size();
-        // vector<int>dp(n+1,-1);
-        // return f(n-1,nums,dp);
-        // return i < size(A) ? max(rob(A, i+1), A[i] + rob(A, i+2)) : 0;
-        vector<int> dp(size(nums),-1);
-        return f(nums, dp, 0);
+    int rob(vector<int>& nums) 
+    {
+        int n = nums.size();
+        dp.assign(n+1,-1);
+        return f(n-1,nums);
     }
 };
