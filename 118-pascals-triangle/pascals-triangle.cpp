c9c024c16d66970725190a37bcc618e7f8804bc3
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-08-02 19:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.87%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -4,7 +4,7 @@ public:
     {
         int n = numRows;
         vector<vector<int>> r(n);
-        for(int i=0;i<numRows;i++)
+        for(int i=0;i<n;i++)
         {
             r[i].resize(i+1);
             r[i][0] = r[i][i] =1;
@@ -12,8 +12,7 @@ public:
             {
                 r[i][j] = r[i-1][j-1] + r[i-1][j];
                 
-            }
-            
+            }  
         }
         return r;
     }
