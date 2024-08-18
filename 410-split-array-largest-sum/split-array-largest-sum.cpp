b7984f61979e38849ca6d6ca01a2e45763161d8d
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-08-18 15:54:50 +0530

    Time: 5 ms (11.45%) | Memory: 9.5 MB (22.88%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    int check(int sum,vector<int>&nums,int k)
-    {
-        int n=nums.size();
-        int painters_spawned=0;
-        int last_sum=0;
-        for(int i=0;i<n;i++)
-        {
-            if(last_sum>=nums[i])
-                last_sum -= nums[i];
-            else
-            {
-                painters_spawned++;
-                last_sum=sum;
-                if(last_sum>=nums[i])
-                    last_sum -= nums[i];
-                else
-                    return 0;
+    int countPartitions(vector<int> &a, int maxSum) {
+        int n = a.size(); //size of array.
+        int partitions = 1;
+        long long subarraySum = 0;
+        for (int i = 0; i < n; i++) {
+            if (subarraySum + a[i] <= maxSum) {
+                //insert element to current subarray
+                subarraySum += a[i];
+            }
+            else {
+                //insert element to next subarray
+                partitions++;
+                subarraySum = a[i];
             }
         }
-        return painters_spawned<=k;
+        return partitions;
     }
-    int splitArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(n==1)
-            return 0;
-        int low=1;
-        int high=accumulate(nums.begin(),nums.end(),0);
-        int ans=-1;
-        while(low<=high)
-        {
-            int mid = low+(high-low)/2;
-            if(check(mid,nums,k))
-            {
-                ans=mid;
-                high=mid-1;
+    int splitArray(vector<int>& a, int k) {
+        int low = *max_element(a.begin(), a.end());
+        int high = accumulate(a.begin(), a.end(), 0);
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(a, mid);
+            if (partitions > k) {
+                low = mid + 1;
             }
-            else
-                low=mid+1;
+            else {
+                high = mid - 1;
+            }
         }
-        return ans;
+        return low;
     }
+
 };
