aa9b675b05982488b0cea9681e4138b015e01a1b
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-10-12 00:57:22 +0530

    Time: 12 ms (78.34%) | Memory: 21.6 MB (91.71%) - LeetSync

---
+++ 3345-find-the-sum-of-the-power-of-all-subsequences/find-the-sum-of-the-power-of-all-subsequences.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    const int mod = 1e9+7;
+    int n;
+    int dp[201][201];
+    int rec(int level,int k,vector<int>&arr)
+    {
+        //pruning
+        // if(level>=n)
+        // {
+        //     return 0;
+        // }
+        //base case
+        if(level==n)
+        {
+            if(k==0)return 1;
+            else return 0;
+        }
+        //cache check
+        if(dp[level][k]!=-1)return dp[level][k];
+        //compute
+        int ans =0;
+        int not_take = rec(level+1,k,arr)%mod;
+        int take=0;
+        if(k>=arr[level])take = rec(level+1,k-arr[level],arr)%mod;
+
+        //save and return 
+        return dp[level][k]= ((take)%mod+(2*not_take)%mod)%mod;
+    }
+    int sumOfPower(vector<int>& nums, int k) {
+        n = nums.size();
+        memset(dp,-1,sizeof(dp));
+        return rec(0,k,nums)%mod;
+    }
+};
