98157a96e6f8d0f4fe9448428819d6baf6e7c060
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-10-12 00:24:34 +0530

    Time: 104 ms (44.95%) | Memory: 21.8 MB (71.43%) - LeetSync

---
+++ 3354-replace-question-marks-in-string-to-minimize-its-value/replace-question-marks-in-string-to-minimize-its-value.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minimizeStringValue(string s) {
+        int n = s.length();
+        vector<int>freq(26,0);
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='?')cnt++;
+            else freq[s[i]-'a']++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(int i=0;i<26;i++)
+        {
+            pq.push({freq[i],i});
+        }
+        string add="";
+        while(cnt--)
+        {
+            auto cur = pq.top();
+            pq.pop();
+            add.push_back(cur.second+'a');
+            cur.first++;
+            pq.push(cur);
+        }
+        sort(add.begin(),add.end());
+        int j =0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='?')
+            {
+                s[i]=add[j++];
+            }
+        }
+        return s;
+    }
+};
