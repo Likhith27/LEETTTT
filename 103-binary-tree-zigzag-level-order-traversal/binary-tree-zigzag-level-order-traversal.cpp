86c3fc7a5c91544d52c29a9985e142c392f02365
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-11-22 16:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (57.79%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -16,27 +16,23 @@ public:
         if(root==NULL)return v;
         queue<TreeNode*>q;
         q.push(root);
-
+        int flag =0;
         while(!q.empty())
         {
             int n = q.size();
-            vector<int>t;
+            vector<int>t(n);
+           
             for(int i=0;i<n;i++)
             {
                 auto cur = q.front();
                 q.pop();
-
+                if(!flag)t[i]=cur->val;
+                else t[n-i-1]=cur->val;
                 if(cur->left)q.push(cur->left);
                 if(cur->right)q.push(cur->right);
-                t.push_back(cur->val);
             }
+            flag=!flag;
             v.push_back(t);
-        }
-        int cnt=0;
-        for(auto &i:v)
-        {
-            cnt++;
-            if(cnt%2==0)reverse(i.begin(),i.end());
         }
         return v;
     }
