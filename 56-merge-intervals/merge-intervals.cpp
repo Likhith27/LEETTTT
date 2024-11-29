0880c443dfda0addfa88469a5327d072f65836c0
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-11-29 15:40:47 +0530

    Time: 4 ms (70.11%) | Memory: 23.9 MB (25.93%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -9,19 +9,19 @@ public:
         }
         sort(intervals.begin(),intervals.end());
         vector<int>last_end = intervals[0];
-
-        for(auto range:intervals)
+        // int n = intervals.size();
+        for(int i=1;i<n;i++)
         {
             //case-I if(next intervals start is less than or equal to first interval end)
-            if(range[0]<=last_end[1])
+            if(intervals[i][0]<=last_end[1])
             {
-                last_end[1]=max(last_end[1],range[1]);
+                last_end[1]=max(last_end[1],intervals[i][1]);
             }
             //case-2(if they don't overlap then directly add to the range list)
             else
             {
                 v.push_back(last_end);
-                last_end = range;
+                last_end = intervals[i];
             }
         }
         v.push_back(last_end);
