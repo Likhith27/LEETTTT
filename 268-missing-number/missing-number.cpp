98022ea3fcbcc3fa83bf37947e45c1d254748f54
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-11-29 11:49:14 +0530

    Time: 28 ms (5.23%) | Memory: 29.5 MB (5.02%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        int k = (n*(n+1))>>1;
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        return k-sum;
+        //mex
+        set<int>s(nums.begin(),nums.end());
+        int mex=0;
+        while(s.find(mex)!=s.end()){
+            mex++;
+        }
+        return mex;
     }
 };
