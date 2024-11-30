895f4f58d67ab530bed623a8aa88b9a4561f5d4c
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-11-30 17:28:10 +0530

    Time: 4 ms (23.34%) | Memory: 16.8 MB (16.16%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        while(num.length()>0 and (num.back()-'0')%2==0)
+         while(num.length()>0 and (num.back()-'0')%2==0)
             num.pop_back();
         return num;
+
     }
 };
