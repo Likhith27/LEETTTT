821aa786b927f7973a9c4b7efc97701d23284fc4
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-11-30 15:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.41%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    int check(int x,int k,vector<int>&arr)
-    {
-        int n = arr.size();
-        int no_of_painters=0;
-        int last_sum=0;
+    int check(int x,int k,vector<int>&nums){
+        int sum=0;
+        int cnt =0;
+        int n = nums.size();
         for(int i=0;i<n;i++)
         {
-            if(last_sum>=arr[i])
+            if(nums[i]<=sum)
             {
-                last_sum-=arr[i];
+                sum-=nums[i];
             }
             else
             {
-                no_of_painters++;
-                last_sum=x;
-                if(last_sum>=arr[i])
-                {
-                    last_sum-=arr[i];
-                }
+                cnt++;
+                sum=x;
+                if(nums[i]<=sum)
+                    sum-=nums[i];
                 else
                     return 0;
             }
         }
-        return no_of_painters<=k;
+        return cnt<=k;
     }
-    
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(), nums.end());
-        int high = accumulate(nums.begin(), nums.end(), 0);
-        int ans = 0;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (check(mid, k, nums)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int n = nums.size();
+        int low = *min_element(nums.begin(),nums.end());
+        int high = accumulate(nums.begin(),nums.end(),0);
+        int ans = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(check(mid,k,nums)){
+                ans =mid;
+                high = mid-1;
             }
+            else
+                low = mid+1;
         }
         return ans;
     }
