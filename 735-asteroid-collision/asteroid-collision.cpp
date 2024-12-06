879f044e3c091762bed985db44d7467e35211925
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-12-06 19:51:58 +0530

    Time: 3 ms (33.60%) | Memory: 22.2 MB (6.55%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& arr) {
-        int n = arr.size();
         stack<int>st;
-        //8, 9, 6, -7, -9, 12, 50, -34
-        for(int i=0;i<n;i++)
-        {
-            //stack empty or positive
-            if(st.empty()||arr[i]>0)
-                st.push(arr[i]);
-            
-            else 
+        //push all the positive things into stack
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            //push all the positive things into stack
+            if(st.empty()||arr[i]>0)st.push(arr[i]);
+            else
             {
+                //we now have negatives in the array we compare with values
+                //in the stack 
                 while(!st.empty() and st.top()>0 and st.top()<abs(arr[i]))
-                    st.pop(); //st.top() is positive
-                
-                if(!st.empty() and st.top()==abs(arr[i]))
                     st.pop();
+                if(!st.empty() and st.top()==abs(arr[i]))st.pop();
                 else
                 {
-                    if(st.empty() || st.top()<0)
+                    if(st.empty()||st.top()<0)
                         st.push(arr[i]);
                 }
-                
             }
         }
         vector<int>v;
-        while(!st.empty())
-        {
-            v.push_back(st.top());
-            st.pop();
-        }
-        reverse(v.begin(),v.end());
-        return v;
+            while(!st.empty()){
+                v.push_back(st.top());
+                st.pop();
+            }
+            reverse(v.begin(),v.end());
+            return v;
     }
 };
