5eaaf4450d429ce7d9c323ea087c09027ed518b8
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-12-07 00:24:45 +0530

    Time: 34 ms (50.94%) | Memory: 139.1 MB (37.80%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    struct deq
-    {
+    struct monotone_deque{
         deque<int>dq;
-    
-        void insert(int x)
+        void push(int x)
         {
-            while(!dq.empty() and dq.back()<x)
-                dq.pop_back();
-            dq.push_back(x);
+           while(!dq.empty() and dq.back()<x){
+             dq.pop_back();
+           }
+           dq.push_back(x);
         }
-
-        void remove(int x)
-        {
+        void pop(int x){
             if(dq.front()==x)
                 dq.pop_front();
         }
-
-        int getMax()
-        {
+        int get_maxi(){
             return dq.front();
         }
     };
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
-    {
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
-        deq dq;
-        for(int i=0;i<k;i++)
-        {
-            dq.insert(nums[i]);
+        int maxi = -1e9;
+        monotone_deque mt;
+        for(int i=0;i<k;i++){
+            mt.push(nums[i]);
         }
         vector<int>v;
-        v.push_back(dq.getMax());
-        for(int i=0;i<n-k;i++)
-        {
-            dq.insert(nums[i+k]);
-            dq.remove(nums[i]);
-            v.push_back(dq.getMax());
+        maxi = mt.get_maxi();
+        v.push_back(maxi);
+        for(int i=0;i<n-k;i++){
+            mt.push(nums[i+k]);
+            mt.pop(nums[i]);
+            v.push_back(mt.get_maxi());
         }
         return v;
     }
