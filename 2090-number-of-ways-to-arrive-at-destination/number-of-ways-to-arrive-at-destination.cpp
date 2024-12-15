0b63fc5c0220e9a018c310a232141f9899fb8a36
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-12-15 23:16:30 +0530

    Time: 4 ms (88.46%) | Memory: 35 MB (73.45%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,44 +1,44 @@
-// #define int long long
 class Solution {
 public:
+    const int mod = 1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int,int>>>g;
+        g.resize(n);
+        for(auto &it:roads){
+            int a = it[0];
+            int b = it[1];
+            int c = it[2];
+            g[a].push_back({b,c});
+            g[b].push_back({a,c});
+        }   
         vector<long long int>dis(n,1e18);
-        vector<long long int>paths(n,0);
+        vector<long long int>cnt(n,1);
         vector<int>vis(n,0);
-        vector<vector<pair<int,int>>>g(n);
-        const int mod = 1e9+7;
-        for(auto edge:roads)
-        {
-            g[edge[0]].push_back({edge[1],edge[2]});
-            g[edge[1]].push_back({edge[0],edge[2]});
-        }
         priority_queue<pair<long long int,int>>pq;
+        pq.push({-0ll,0});
         dis[0]=0;
-        paths[0]=1;
-        pq.push({-0LL,0});
-        while(!pq.empty())
-        {
+        cnt[0]=1;
+        while(!pq.empty()){
             auto cur = pq.top();
             pq.pop();
+
             long long int d = -cur.first;
             int node = cur.second;
-            // if(vis[node])continue;
-            // vis[node]=1;
-            if(d>1LL*dis[node])continue;
-            for(auto v:g[node])
-            {
-                if(dis[v.first]>dis[node]+v.second)
-                {
+            if(vis[node])continue;
+            vis[node]=1;
+            if(d>1ll*dis[node])continue;
+            for(auto v:g[node]){
+                if(dis[v.first]>dis[node]+v.second){
                     dis[v.first]=dis[node]+v.second;
-                    paths[v.first]=paths[node]%mod;
+                    cnt[v.first]=cnt[node];
                     pq.push({-dis[v.first],v.first});
                 }
-                else if(dis[v.first]==dis[node]+v.second)
-                {
-                    paths[v.first] = (paths[v.first]+paths[node])%mod;
+                else if(dis[v.first]==dis[node]+v.second){
+                    cnt[v.first]=(cnt[v.first]+cnt[node])%mod;
+                    cnt[v.first]%=mod;
                 }
             }
         }
-        return paths[n-1]%mod;
+        return cnt[n-1]%mod;
     }
 };
