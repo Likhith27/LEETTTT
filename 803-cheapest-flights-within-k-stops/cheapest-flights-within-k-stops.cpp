6f575eb609910e969c4118e9dcfa8ae644c851ea
Likhith Kumar Lakkireddy <likhith.kumar@iiitb.org>
2024-12-15 22:46:56 +0530

    Time: 18 ms (11.64%) | Memory: 19 MB (14.89%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -3,7 +3,7 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         // pq state : {cost, {node, tickets}}
         //state:{node,tickets}
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+        priority_queue<pair<int,pair<int,int>>> pq;
         vector<vector<int>> dis(n, vector<int>(k + 2, INT_MAX));
         vector<vector<pair<int, int>>> g(n);
         
@@ -13,25 +13,27 @@ public:
         }
         
         dis[src][0] = 0;
-        pq.push({0, {src, 0}});
+        pq.push({-0, {src, 0}});
         
         while (!pq.empty()) {
             auto cur = pq.top();
             pq.pop();
             
-            int cost = cur.first;
+            int cost = -cur.first;
             int nn = cur.second.first;
             int stops = cur.second.second;
             
             if (stops > k) continue;
 
-            for (auto v : g[nn]) {
+            for (auto v : g[nn]) 
+            {
                 int nextNode = v.first;
                 int edgeWeight = v.second;
                 
-                if (cost + edgeWeight < dis[nextNode][stops + 1]) {
+                if (dis[nextNode][stops + 1]>cost + edgeWeight) 
+                {
                     dis[nextNode][stops + 1] = cost + edgeWeight;
-                    pq.push({cost + edgeWeight, {nextNode, stops + 1}});
+                    pq.push({-dis[nextNode][stops + 1], {nextNode, stops + 1}});
                 }
             }
         }
